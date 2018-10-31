c7377d179af2c5bc614a67863d22f6e8a1d63566
Rafael Godoy <rarfaelgoodoy@gmail.com>
2018-10-31 17:16:23 -0300

    Changing baseURL at Http.js

--- src/services/Http.js
+++ src/services/Http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'http://localhost/devtest-backend/public/api/v1',
+    baseURL: 'http://your-api-domain.api/api',
     timeout: 9000,
     headers: {'Content-Type': 'application/json'}
 })
