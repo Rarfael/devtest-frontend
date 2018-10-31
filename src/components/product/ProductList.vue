4c1b4f1d2f02fd6ed4b32f0c7348d7cc8c960c8d
Rafael Godoy <rarfaelgoodoy@gmail.com>
2018-10-30 21:45:50 -0300

    Creating a card component and a list component for products

---
+++ src/components/product/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<ProductCard v-bind="{product}"/>
+</div>
+</template>
+<script>
+    import ProductCard from './ProductCard.vue'
+    export default {
+        name: 'ProductList',
+        components: {
+            ProductCard
+        },
+        data: function () {
+            return {
+                product: {
+                    product_name: 'Nome do Produto',
+                    product_type: 'Tipo do produto',
+                    product_description: 'Descrição do Produto'
+                }
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
